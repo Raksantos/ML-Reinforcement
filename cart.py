434793f81eb8bb370894e8215745e914f8be874b
raksantos <rodrigosnt3@gmail.com>
2022-06-28 15:19:37 -0300

    addying notebook and frozen lake algorithm

--- cart.py
+++ cart.py
@@ -1,15 +1,15 @@
 import gym
 import random
 import numpy as np
+from gym.envs.registration import register
 
 enviorments = {
-    'CartPole-v0': gym.make('CartPole-v0'),
     'CartPole-v1': gym.make('CartPole-v1'),
     'MountainCar-v0': gym.make('MountainCar-v0'),
     'MountainCarContinuous-v0': gym.make('MountainCarContinuous-v0'),
     'Pendulum-v1': gym.make('Pendulum-v1'),
     'Acrobot-v1': gym.make('Acrobot-v1'),
-    'FrozenLake-v1': gym.make('FrozenLake-v1')
+    'FrozenLake-v1': gym.make('FrozenLake-v1', is_slippery=False)
 }
 
 class Agent():
@@ -39,6 +39,34 @@ class Agent():
       action = np.random.uniform(self.action_low, self.action_high, self.action_shape)
 
     return action
+
+class QAgent(Agent):
+  def __init__(self, env, discount_rate=0.97, learning_rate=0.01):
+    super().__init__(env)
+    self.state_size = env.observation_space.n
+
+
+    self.discount_rate = discount_rate
+    self.learning_rate = learning_rate
+    self.build_model()
+
+  def build_model(self):
+    self.q_table = 1e-4 * np.random.random([self.state_size, self.state_action_size])
+
+  def get_action(self, state):
+    q_state = self.q_table[state]
+    action = np.argmax(q_state)
+    return action
+
+  def train(self, experience):
+    state, action, next_state, reward, done = experience
+
+    q_next = self.q_table[next_state]
+    q_next = np.zeros([self.action_size]) if done else q_next
+    q_target = reward + self.discount_rate * np.max(q_next)
+
+    q_update = q_target - self.q_table[state, action]
+    self.q_table[state, action] += self.learning_rate * q_update
 
 if __name__ == "__main__":
     env = enviorments['FrozenLake-v1']
